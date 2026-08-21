f21b1befe3ed7fc0acb764786e8ad10706b6457c
Adroph <adroph2605@gmail.com>
2026-08-21 22:07:56 +0300

    Add ThemeProvider and BranchProvider to main.jsx

--- wajasilimali/frontend/src/main.jsx
+++ wajasilimali/frontend/src/main.jsx
@@ -3,15 +3,21 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { AuthProvider } from './contexts/AuthContext';
+import { ThemeProvider } from './contexts/ThemeContext';
+import { BranchProvider } from './contexts/BranchContext';
 import './i18n';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
+      <ThemeProvider>
+        <AuthProvider>
+          <BranchProvider>
+            <App />
+          </BranchProvider>
+        </AuthProvider>
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
